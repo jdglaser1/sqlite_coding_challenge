82c242326e2da1b20f8a5279a8c064a5a49d84a8
Joshua Glaser <jdglaser@usf.edu>
2025-11-14 12:59:52 -0500

    Task 1

--- challenge.sql
+++ challenge.sql
@@ -1,4 +1,13 @@
 --Task 1: Top 5 customer spend
 
-
+SELECT
+    c.first_name,
+    c.last_name,
+    SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+JOIN order_items oi ON o.id = oi.order_id
+GROUP BY c.id
+ORDER BY total_spend DESC
+LIMIT 5;
 
