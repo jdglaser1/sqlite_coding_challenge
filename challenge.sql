903c91edf3f3d0413d6010cd02e7eabf69d77bd3
Joshua Glaser <jdglaser@usf.edu>
2025-11-15 18:29:37 -0500

    Started task 3, sorted by avg salary

--- challenge.sql
+++ challenge.sql
@@ -13,9 +13,22 @@
 
 -- Task 2: Total Revenue by Product Category
 
-SELECT p.category,
-SUM(oi.quantity * oi.unit_price) AS revenue
-FROM order_items oi
-JOIN products p ON oi.product_id = p.id
-GROUP BY p.category
-ORDER BY revenue DESC
+-- SELECT p.category,
+-- SUM(oi.quantity * oi.unit_price) AS revenue
+-- FROM order_items oi
+-- JOIN products p ON oi.product_id = p.id
+-- GROUP BY p.category
+-- ORDER BY revenue DESC
+
+
+-- Task 3- Employees earning above their department average
+
+SELECT department_id, AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department_id
+ORDER BY avg_salary DESC
+
+
+SELECT e.first_name, e.last_name, e.salary
+
+-- Task 4- Cities with Most Loyal Customers
