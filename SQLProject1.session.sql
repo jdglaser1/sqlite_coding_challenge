cbed3ec033a09e087e1927183b7ab5c59ea2f4d3
Joshua Glaser <jdglaser@usf.edu>
2025-11-15 17:49:32 -0500

    Task 2

---
+++ SQLProject1.session.sql
@@ -0,0 +1,9 @@
+-- -- Experimenting here
+
+-- SELECT first_name, last_name, salary,
+-- round(salary * 1.1,3) AS SalaryWithBonus
+-- FROM employees
+
+
+SELECT MAX(salary)
+FROM employees
--- challenge.sql
+++ challenge.sql
@@ -1,13 +1,21 @@
 --Task 1: Top 5 customer spend
 
-SELECT
-    c.first_name,
-    c.last_name,
-    SUM(oi.quantity * oi.unit_price) AS total_spend
-FROM customers c
-JOIN orders o ON c.id = o.customer_id
-JOIN order_items oi ON o.id = oi.order_id
-GROUP BY c.id
-ORDER BY total_spend DESC
-LIMIT 5;
+-- SELECT
+--     c.first_name,
+--     c.last_name,
+--     SUM(oi.quantity * oi.unit_price) AS total_spend
+-- FROM customers c
+-- JOIN orders o ON c.id = o.customer_id
+-- JOIN order_items oi ON o.id = oi.order_id
+-- GROUP BY c.id
+-- ORDER BY total_spend DESC
+-- LIMIT 5;
 
+-- Task 2: Total Revenue by Product Category
+
+SELECT p.category,
+SUM(oi.quantity * oi.unit_price) AS revenue
+FROM order_items oi
+JOIN products p ON oi.product_id = p.id
+GROUP BY p.category
+ORDER BY revenue DESC
