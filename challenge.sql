b22bcc7105d080ad14b05bf24851dab1b29e6645
Joshua Glaser <jdglaser@usf.edu>
2025-11-15 22:58:45 -0500

    Got task 1 and 4 to work

--- challenge.sql
+++ challenge.sql
@@ -1,15 +1,20 @@
 --Task 1: Top 5 customer spend
 
--- SELECT
---     c.first_name,
---     c.last_name,
---     SUM(oi.quantity * oi.unit_price) AS total_spend
--- FROM customers c
--- JOIN orders o ON c.id = o.customer_id
--- JOIN order_items oi ON o.id = oi.order_id
--- GROUP BY c.id
--- ORDER BY total_spend DESC
--- LIMIT 5;
+SELECT
+    c.first_name,
+    c.last_name,
+    SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+JOIN order_items oi ON o.id = oi.order_id
+GROUP BY c.id
+ORDER BY total_spend DESC
+LIMIT 5;
+--Functioning
+
+
+
+
 
 -- Task 2: Total Revenue by Product Category
 
@@ -23,12 +28,20 @@
 
 -- Task 3- Employees earning above their department average
 
-SELECT department_id, AVG(salary) AS avg_salary
-FROM employees
-GROUP BY department_id
-ORDER BY avg_salary DESC
+-- SELECT department_id, AVG(salary) AS avg_salary
+-- FROM employees
+-- GROUP BY department_id
+-- ORDER BY avg_salary DESC
 
 
-SELECT e.first_name, e.last_name, e.salary
 
--- Task 4- Cities with Most Loyal Customers
+-- -- Task 4- Cities with Most Loyal Customers
+-- -- This one seemed a bit confusing being that there was only 4 gold members and all of which are in Tampa
+-- -- If there were more it would display
+
+SELECT city, COUNT(*) AS gold_members
+FROM customers
+WHERE loyalty_level ='Gold'
+GROUP BY city
+ORDER BY gold_members DESC
+--Functioning
